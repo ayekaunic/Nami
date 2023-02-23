9439eeff6188060107a063685370a61ce8a6ec9f
Muhammad Aun <81079827+ayekaunic@users.noreply.github.com>
2023-02-24 01:36:27 +0500

    Add files via upload

--- nami.py
+++ nami.py
@@ -5,6 +5,9 @@ import wikipedia
 import pyttsx3
 import time
 
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
 # initializing recognizer
 ears = sr.Recognizer()
 
@@ -13,27 +16,35 @@ nami = pyttsx3.init()
 
 # asking for user's name
 name = ''
-print("hello. i am Nami. your virtual assistant. what is your name?")
+print("\nhello. i am Nami. your virtual assistant. what is your name?")
 nami.say("hello. i am Nami. your virtual assistant. what is your name?")
 nami.runAndWait()
 while True:
-    print("listening...")
+    print("\nlistening...")
     try:
         with sr.Microphone() as source:
             ears.adjust_for_ambient_noise(source)
             audio = ears.listen(source, timeout = 81)
             name = ears.recognize_google(audio)
-            name = name.split()[-1]     
+            name = name.split()[-1]
+            print(f"{CLEAR}")
+            print(f"{CLEAR_AND_RETURN}")  
             break           
     except sr.UnknownValueError:
+        print(f"{CLEAR}")
+        print(f"{CLEAR_AND_RETURN}")
         nami.say("sorry. i didn't get that. come again?")
         nami.runAndWait()
     except sr.WaitTimeoutError:
+        print(f"{CLEAR}")
+        print(f"{CLEAR_AND_RETURN}")
         nami.say("please tell me what your name is.")
         nami.runAndWait()
 
 # greeting
-greeting = f"nice to meet you {name}. how can i help you? i can perform google searches and wikipedia lookups."
+print(f"{CLEAR}")
+print(f"{CLEAR_AND_RETURN}")
+greeting = f"\nnice to meet you {name}. how can i help you? i can perform google searches and wikipedia lookups."
 print(greeting)
 nami.say(greeting)
 nami.runAndWait()
@@ -41,18 +52,21 @@ nami.runAndWait()
 # ask user for input
 while True:
     anything_else = True
-    print("listening...")
+    print("\nlistening...")
     try:
         with sr.Microphone() as source:
             ears.adjust_for_ambient_noise(source)
             audio = ears.listen(source, timeout = 81)
             text = ears.recognize_google(audio)
+            print(f"{CLEAR}")
+            print(f"{CLEAR_AND_RETURN}")
             
-            # perform action based on user input
+        # perform action based on user input
     
         # if you want to perform a google search for 'xyz', say 'search google for xyz'
         if "google" in text.lower():
             search_term = text.split("for")[1]
+            print(f"\nsearching google for: {search_term}")
             nami.say(f"searching google for: {search_term}")
             nami.runAndWait()
             url = "https://google.com/search?q=" + search_term
@@ -64,35 +78,44 @@ while True:
         # if you want to look up 'xyz' on wikipedia, say 'search wikipedia for xyz'
         elif "wikipedia" in text.lower():
             search_term = text.split("for")[1]
+            print(f"\nlooking up {search_term} on wikipedia")
             nami.say(f"looking up {search_term} on wikipedia")
             nami.runAndWait()
             summary = wikipedia.summary(search_term, sentences = 2)
+            print(f"{CLEAR}")
+            print(f"{CLEAR_AND_RETURN}")
+            print("\nthis is what i've learned: ")
             nami.say("this is what i've learned: ")
             nami.runAndWait()
             time.sleep(1)
+            print(f"\n{summary}")
             nami.say(summary)
             nami.runAndWait()
             
         # if you want to turn off the voice assistant, say 'go to sleep nami'
         elif "go to sleep" in text.lower():
-            print(f"alright {name}, have a good day!")
+            print(f"\nalright {name}, have a good day!")
             nami.say(f"alright {name}, have a good day!")
             nami.runAndWait()
             break
         
+        # if you want it to be on stand by, say 'stay on hold'
         elif "on hold" in text.lower():
-            print(f"okay {name}. i'll be here if you need me. just say, wake up, to wake me up!")
+            print(f"\nokay {name}. i'll be here if you need me. just say, wake up, to wake me up!")
             nami.say(f"okay {name}. i'll be here if you need me. just say, wake up, to wake me up!")
             nami.runAndWait()
             while True:
-                print("listening...")
+                print("\nlistening...")
                 try:
                     with sr.Microphone() as source:
                         ears.adjust_for_ambient_noise(source)
                         audio = ears.listen(source, timeout = 81)
                         query = ears.recognize_google(audio)
+                        print(f"{CLEAR}")
+                        print(f"{CLEAR_AND_RETURN}")
                         if "wake up" in query.lower():
                             anything_else = False
+                            print("\nalright. i'm up. what do you want me to do?")
                             nami.say("alright. i'm up. what do you want me to do?")
                             nami.runAndWait()
                             break
@@ -100,7 +123,8 @@ while True:
                 except sr.WaitTimeoutError: continue
             
         else:
-            nami.say("come again? what do you want me to do?")
+            print(f"\nsorry {name}. i can only perform google searches and wikipedia lookups. try again.")
+            nami.say(f"sorry {name}. i can only perform google searches and wikipedia lookups. try again.")
             nami.runAndWait()
             anything_else = False
         
@@ -111,10 +135,14 @@ while True:
             
     except sr.UnknownValueError:
         time.sleep(1)
-        nami.say("did you say something? sorry. i did not understand. come again?")
+        print(f"{CLEAR}")
+        print(f"{CLEAR_AND_RETURN}")
+        nami.say(f"{name}? did you say something? sorry. i did not understand. come again?")
         nami.runAndWait()
         continue
     except sr.RequestError:
-        nami.say("sorry. having trouble accesing the internet. try again, later.")
+        print(f"{CLEAR}")
+        print(f"{CLEAR_AND_RETURN}")
+        nami.say(f"sorry {name}. i am having trouble accesing the internet. try again, later.")
         nami.runAndWait()
         continue
